562ad03b715a6bf282990df7796e802381e18b7b
Kennius7 <ogbogukenny@yahoo.com>
2025-06-19 12:51:03 +0100

    setting up the product API route - 05

--- api/product.js
+++ api/product.js
@@ -18,7 +18,7 @@ const cleanData = (data) => {
 
 export default async function handler(req, res) {
     console.log("Checking...");
-    console.log("Request API Type:>>>", req.body.apiType);
+    console.log("Request API Type:>>>", req.body);
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
