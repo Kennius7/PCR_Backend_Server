4e772647a4965a8835b21b80d21d3ece3bc1a1a9
Kennius7 <ogbogukenny@yahoo.com>
2025-06-19 18:39:08 +0100

    setting up the auth and user API routes - 01

--- api/product.js
+++ api/product.js
@@ -5,17 +5,6 @@ const { db } = require("../FirebaseConfig.js");
 require('dotenv').config();
 
 
-// Clean the data before updating
-// const cleanData = (data) => {
-//     const cleaned = {};
-//     Object.keys(data).forEach(key => {
-//         if (data[key] !== undefined) {
-//         cleaned[key] = data[key];
-//         }
-//     });
-//     return cleaned;
-// };
-
 export default async function handler(req, res) {
     console.log("Checking...");
     // console.log("Request API Type:>>>", req?.body?.apiType);
@@ -79,104 +68,6 @@ export default async function handler(req, res) {
         } catch (error) {
             console.log("Checking ERROR FETCHING...", res.statusCode, error.message);
             return res.json({ error: `Couldn't fetch Data. Error: ${error.message}` });
-        }
-    }
-
-    // Edit Product Block
-    if (req.method === "POST" && req.body.apiType === "EDITPRODUCTS") {
-        try {
-            const { id, updatedData } = req.body;
-            console.log("ImageURL:>>>>", updatedData.image);
-
-            if (!id || !updatedData.productName || updatedData.newPrice === undefined 
-                || updatedData.oldPrice === undefined || !updatedData.category || updatedData.image.length === 0) {
-                return res.status(400).json({ success: false, error: "Missing required fields!" });
-            }
-
-            const docRef = doc(db, "judyhub-products", "products");
-            const docSnap = await getDoc(docRef);
-
-            if (!docSnap.exists()) {
-                console.log("Document does not exist!");
-                return res.status(400).json({ success: false, error: "Document does not exist!" });
-            }
-
-            const currentData = docSnap.data();
-            if (!currentData.allProducts || !Array.isArray(currentData.allProducts)) {
-                return res.status(400).json({ success: false, error: "Invalid product structure in database!" });
-            }
-            const updatedArray = currentData.allProducts.map((item) =>
-                item.id === id
-                    ? { 
-                        ...item, 
-                        name: updatedData.productName, 
-                        newPrice: updatedData.newPrice, 
-                        oldPrice: updatedData.oldPrice, 
-                        category: updatedData.category, 
-                        tags: updatedData.tags, 
-                        image: updatedData.image, 
-                        description: updatedData.description, 
-                    }
-                    : item
-            );
-
-            await updateDoc(docRef, { allProducts: updatedArray });
-            const message = `Successfully posted product data`;
-            console.log(message);
-            return res.status(200).json({ success: true, message: message });
-        } catch (error) {
-            console.log("Checking POST Method ERROR...", res.statusCode);
-            console.log(error);
-            return res.status(500).json({ error: `Error: ${error.message}` });
-        }
-    }
-
-    // Add New Product Block
-    if (req.method === "POST" && req.body.apiType === "ADDPRODUCT") {
-        try {
-            const { updatedData } = req.body;
-            console.log("ImageURL:>>>>", updatedData.image);
-
-            if (!updatedData.productName || updatedData.newPrice === undefined 
-                || updatedData.oldPrice === undefined || !updatedData.category 
-                || updatedData.image.length === 0 || !updatedData.tags || !updatedData.id) {
-                    console.log("Missing required fields!");
-                return res.status(400).json({ success: false, error: "Missing required fields!" });
-            }
-
-            const docRef = doc(db, "judyhub-products", "products");
-            const docSnap = await getDoc(docRef);
-
-            if (!docSnap.exists()) {
-                console.log("Document does not exist!");
-                return res.status(400).json({ success: false, error: "Document does not exist!" });
-            }
-
-            const currentData = docSnap.data();
-            if (!currentData.allProducts || !Array.isArray(currentData.allProducts)) {
-                console.log("Invalid product structure in database!");
-                return res.status(400).json({ success: false, error: "Invalid product structure in database!" });
-            }
-
-            const updatedArray = [...currentData.allProducts, {
-                name: updatedData.productName, 
-                newPrice: updatedData.newPrice, 
-                oldPrice: updatedData.oldPrice, 
-                category: updatedData.category, 
-                tags: updatedData.tags, 
-                image: updatedData.image, 
-                id: updatedData.id,
-                description: updatedData.description,
-            }];
-
-            await updateDoc(docRef, { allProducts: updatedArray });
-            const message = `Successfully posted product data`;
-            console.log(message);
-            return res.status(200).json({ success: true, message: message });
-        } catch (error) {
-            console.log("Checking POST Method ERROR...", res.statusCode);
-            console.log(error);
-            return res.status(500).json({ error: `Error: ${error.message}` });
         }
     }
 
---
+++ api/user.js
@@ -0,0 +1,89 @@
+
+const { collection, updateDoc, doc, query, where, getDocs } = require("firebase/firestore");
+const { db } = require("../FirebaseConfig.js");
+const jwt = require("jsonwebtoken");
+const admin = require("firebase-admin");
+
+require('dotenv').config();
+
+
+const pcrDatabaseSecretKey = process.env.PCR_DATABASE_SECRET_KEY;
+
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+        databaseURL: 'https://pcr-database.firebaseio.com',
+    });
+    console.log("Firebase Admin Initialized");
+}
+
+
+
+export default async function handler(req, res) {
+    console.log("Checking...");
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        console.log("Checking OPTIONS Method...", res.statusCode);
+        return;
+    }
+
+
+    // Fetch User Data Block
+    if (req.method === "GET" && req?.query?.apiType === "FETCHUSERDATA") {
+        try {
+            const authHeader = req.headers.authorization;
+            console.log("AUTH:>>>>", authHeader);
+
+            if (!authHeader || !authHeader.startsWith("Bearer ")) {
+                console.log("Access Denied: No token provided");
+                return res.status(401).json({ success: false, message: "Access Denied: No token provided" });
+            }
+    
+            const token = authHeader.split("Bearer ")[1];
+    
+            if (!token) {
+                console.log("Access Denied: No token detected!");
+                return res.status(401).json({ success: false, message: "Unauthorized!" });
+            }
+
+            const user = jwt.verify(token, pcrDatabaseSecretKey, (err, user) => {
+                if (err) return res.status(403).send("Invalid Token!");
+                req.user = user;
+                const userData = req.user;
+                return userData;
+            })
+
+            console.log("User: >>>>>", user);
+            const q = query(collection(db, "User_Data"), where("email", "==", user.email));
+            const querySnapshot = await getDocs(q);
+            const filteredData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            console.log("Filtered Data: ", filteredData);
+            const { name, email, number, image, address, cartData } = filteredData[0];
+            const fetchedData = { name: name, email: email, number: number };
+
+            console.log("Fetched Data: >>>", fetchedData);
+            return res.status(200).json({ data: fetchedData, message: "Data was fetched successfully" });
+        } catch (error) {
+            console.log("Checking ERROR FETCHING...", res.statusCode, error.message);
+            return res.status(500).json({ success: false, message: `Couldn't fetch Data. Error: ${error.message}` });
+        }
+    }
+
+    if (!['POST', 'GET', 'OPTIONS'].includes(req.method)) {
+        console.log("Checking NO METHOD SELECTED...", res.statusCode);
+        return res.status(405).json({ success: false, message: 'Method not allowed' });
+    }
+}
+
+
+
+
